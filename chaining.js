03dd0610aba5b08e415b2f63c4b40accfdfdc37a
David Cramb <david.cramb@gmail.com>
2016-02-29 20:14:17 -0600

    fun

--- chaining.js
+++ chaining.js
@@ -1,2 +1,9 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-document.write(integers.sort(function(first, second){return first-second}).reverse().filter(function(bob){return bob < 19}).map(function(mathing){return mathing*1.5 - 1}).reduce(function(ricky, bobby){return ricky+bobby}));
+// document.write(integers.sort(function(first, second){return first-second}).reverse().filter(function(bob){return bob < 19}).map(function(mathing){return mathing*1.5 - 1}).reduce(function(ricky, bobby){return ricky+bobby})); my solution, one line because i'm dumb
+var chain = integers
+.sort(function(a,b){return a-b}) 
+.reverse()//sorts numbers in descending order
+.filter(function(num){return num < 19}) //remove integers greater than 19
+.map(function(num){return (num * 1.5) - 1 }) // multiplies all numbers by 1.5 and subtracts 1
+.reduce(function(previous, current) { return previous + current}); // adds all numbers together
+console.log("result", chain);
--- markdown-intro.md
+++ markdown-intro.md
@@ -3,4 +3,32 @@
 ### im an h3
 #### guess
 ##### what
-###### i amt
+###### i am
+
+*I'm Italics*  
+_I'm also italics_  
+
+**I'm bold**  
+__I'm also bold__  
+
+~~cross this out~~
+
+1.  first
+    * something important
+2.  second
+    - second important thing
+3.  third
+
+<h3>h3 works too</h3>
+<p>Super important paragraph</p>
+
+```
+var cat = 'meow';
+var dog = 'woof'
+var iguana = 'what';
+```
+
+> i'm a really  nice  
+> block quote
+
+[click for google](http://www.google.com)
